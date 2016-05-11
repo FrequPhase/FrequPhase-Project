6124433dc99ee7bc50226ed056d726a05468bf82
Paul Grosen <pcgrosen@gmail.com>
2016-05-11 11:50:37 -0700

    Added colors param to makeGrad

--- src/main.js
+++ src/main.js
@@ -4,21 +4,22 @@ class Main {
     constructor() {
         this.mainCanvas = new AbstractCanvas("main-canvas");
         this.gradBase = 0;
-        this.mainCanvas.ctx.fillStyle = this.makeGrad();
+        this.mainCanvas.ctx.fillStyle = this.makeGrad(["red", "orange", "yellow", "green", "blue", "purple"]);
         this.mainCanvas.ctx.fillRect(0, 0, 500, 500);
         window.requestAnimationFrame(() => this.render());
     }
 
-    makeGrad() {
+    makeGrad(colors) {
         // Makes a gradient that extends far beyond the screen. This allows for a smooth transition from
         // the end of the screen in the bottom right back to the top left. Otherwise, the colors would pop back and
         // forth, because the canvas API doesn't support wrapping.
+        colors = colors || ["red", "green", "blue"];
         let grad = this.mainCanvas.ctx.createLinearGradient(-500, -500,  1000, 1000);
         var colorLoc = this.gradBase;
         for (let i = 0; i < 3; i++) {
-            grad.addColorStop(colorLoc % 1, "red");
-            grad.addColorStop((colorLoc + (1/9)) % 1, "green");
-            grad.addColorStop((colorLoc + (2/9)) % 1, "blue");
+            for (let j = 0; j < colors.length; j++) {
+                grad.addColorStop((colorLoc + (j / (3 * colors.length))) % 1, colors[j]);
+            }
             colorLoc = (colorLoc + (1/3)) % 1;
         }
         return grad;
@@ -26,8 +27,8 @@ class Main {
 
     render() {
         this.mainCanvas.ctx.clearRect(0, 0, 500, 500);
-        this.gradBase += 0.001;
-        this.mainCanvas.ctx.fillStyle = this.makeGrad();
+        this.gradBase += 0.01;
+        this.mainCanvas.ctx.fillStyle = this.makeGrad(["red", "orange", "yellow", "green", "blue", "purple"]);
         this.mainCanvas.ctx.fillRect(0, 0, 500, 500);
         window.requestAnimationFrame(() => this.render());
     }
