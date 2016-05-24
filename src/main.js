8adc154fe3daa79f75c777675a061a51919c85b6
Honulaz <honulaz@gmail.com>
2016-05-24 11:18:36 -0700

    more sliders and radio buttons

--- src/main.js
+++ src/main.js
@@ -23,6 +23,9 @@ class Main {
             this.mainCanvas.width / 2, this.mainCanvas.height / 2,
             this.mainCanvas, (this.images.pinwheel.width / 2) - 50);
 
+        document.getElementById("tone-button").addEventListener("onclick", () => this.setOutputTypeTone());
+        document.getElementById("mp3-button").addEventListener("onclick", () => this.setOutputTypeMp3());
+
         window.requestAnimationFrame(() => this.render());
     }
 
@@ -44,11 +47,20 @@ class Main {
         this.observerVec = new Vector(900, 700);
         this.speakerVec = new Vector((Math.cos(this.rick.angularLoc) * this.rick.radius) + (this.mainCanvas.width / 2), (Math.sin(this.rick.angularLoc) * this.rick.radius) + (this.mainCanvas.height / 2));
         this.posVec = this.observerVec.sub(this.speakerVec);
-        console.log(dopplerShift(0, this.speakerVel.component(this.posVec) * SCALE, 100, 343));
+        console.log(dopplerShift(0, this.speakerVel.component(this.posVec) * SCALE, +document.getElementById('origin-frequency').value, 343));
         this.mainCanvas.ctx.drawImage(this.images.person, this.observerVec.x, this.observerVec.y);
         //console.log(this.speakerVel.component(this.posVec) * SCALE);
         this.rick.updateImage(this.getSpeakerAngularVelocity());
+        console.log(document.getElementById("tone").checked);
         window.requestAnimationFrame(() => this.render());
+    }
+
+    setOutputTypeTone() {
+
+    }
+
+    setOutputTypeMp3() {
+        
     }
 
     getSpeakerAngularVelocity() {
